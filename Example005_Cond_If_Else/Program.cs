b3266997d30a906061f11fe5617db87e4307fab9
Kranz <zzulluzz@outlook.com>
2023-09-17 13:37:23 +0200

    add example 005 - If Else task in 005 dir

---
+++ Example005_Cond_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Va")
+{
+    Console.WriteLine("Hooray, it's Va!");
+} 
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
